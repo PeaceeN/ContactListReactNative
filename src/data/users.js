70376fea1f7ead8bb3c2fe413197e269d2863673
felipepiuci <felipemiola2010@hotmail.com>
2021-01-20 17:29:06 -0300

    first commit

---
+++ src/data/users.js
@@ -0,0 +1,23 @@
+export default [
+    {
+        id: 1,
+        name: 'João Marcelo',
+        email: 'joaomarcelo@empmail.com',
+        avatarUrl:
+            'https://cdn.pixabay.com/photo/2016/09/01/08/24/smiley-1635449__340.png'
+    },
+    {
+        id: 2,
+        name: 'Nicolas Pedro',
+        email: 'nicolaspedro@empmail.com',
+        avatarUrl:
+            'https://cdn.pixabay.com/photo/2016/09/01/08/24/smiley-1635449__340.png'
+    },
+    {
+        id: 3,
+        name: 'Lucas Maciel',
+        email: 'lucasmaciel@empmail.com',
+        avatarUrl:
+            'https://cdn.pixabay.com/photo/2016/09/01/08/24/smiley-1635449__340.png'
+    },
+];
---
+++ src/views/Userlist.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import { View, Text , FlatList, Alert } from 'react-native'
+import { ListItem, Button } from 'react-native-elements'
+import { Icon } from 'react-native-vector-icons'
+import users from '../data/users'
+
+export default props => {
+
+    function confirmUserDeletion(user) {
+        Alert.alert('Excluir Usuário', 'Deseja Excluir Usuário?', [
+            {
+                text: 'Sim',
+                onPress() {
+                    console.warn('delete' + user.id)
+                }
+            },
+            {
+                text: 'Não'
+            }
+        ])
+    }
+
+    function getActions(user) {
+        return (
+            <>
+                <Button
+                    onPress={() => props.navigation.navigate('UserForm', user)}
+                    type="clear"
+                    icon={<Icon name="edit" size={25} color="orange" />}
+                />
+                <Button
+                    onPress={() => confirmUserDeletion(user)}
+                    type="clear"
+                    icon={<Icon name="delet" size={25} color="red" />}
+                />
+            </>
+        )
+    }
+
+    function getUserItem({ item: user }) {
+        return (
+            <ListItem
+                leftAvatar={{source: {uri: user.avatarUrl}}}
+                key={user.id}
+                title={user.name}
+                subtitle={user.email}
+                bottomDivider
+                rightElement={getActions(user)}
+                onPress={() => props.navigation.navigate('UserForm', user)}
+            />
+        )
+    }
+
+    return (
+        <View>
+            <FlatList
+                keyExtractor={user => user.id.toString()}
+                    data={users}
+                    renderItem={getUserItem}
+            />
+        </View>
+    )
+}
